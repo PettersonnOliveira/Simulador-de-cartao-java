8e27d806bdb5e3ab7b7f62657e3024ba578998ea
PettersonnOliveira <devpettersonn@gmail.com>
2025-05-15 20:08:43 -0300

    Initial commit

---
+++ CartaoDeCredito.java
@@ -0,0 +1,38 @@
+package Compras;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartaoDeCredito {
+    private Double limite;
+    private Double saldo;
+    private List<Compra> compras;
+
+    public CartaoDeCredito(Double limite) {
+        this.limite = limite;
+        this.saldo = limite;
+        this.compras = new ArrayList<>();
+    }
+
+    public boolean lancaCompra(Compra compra){
+        if (this.saldo > compra.getValor()){
+            this.saldo -= compra.getValor();
+            this.compras.add(compra);
+            return true;
+        }
+
+        return false;
+    }
+
+    public Double getLimite() {
+        return limite;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public List<Compra> getCompras() {
+        return compras;
+    }
+}
